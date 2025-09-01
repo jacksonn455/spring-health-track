02e1a6771bbb80c2efccc76e858a5a05efe35dda
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2025-09-01 12:08:06 -0300

    create project

---
+++ src/main/java/com/springhealthtrack/api/SpringHealthTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.springhealthtrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHealthTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHealthTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springhealthtrack/api/SpringHealthTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhealthtrack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHealthTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
